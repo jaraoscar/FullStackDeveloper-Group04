61042402be4e01690636a9d8fb0553091f17b7f5
Gladysch <gchecasaca@lexuseditores.com>
2018-03-17 12:03:33 -0500

    Clase07

--- Angular/Clase07/appFormularioValidacion/src/app/app.component.ts
+++ Angular/Clase07/appFormularioValidacion/src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface iUsuario {
+  nombre: string,
+  apellido: string,
+  correo: string,
+  contrasena: string
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild("f") registro: NgForm
+
+  registrar() {
+    const datosUsuario: iUsuario = {
+        nombre: this.registro.value.nombre, 
+        apellido: this.registro.value.apellido, 
+        correo: this.registro.value.correo, 
+        contrasena: this.registro.value.contrasena
+    }
+
+    console.log(this.registro)
+  }
 }
--- Angular/Clase07/appFormularioValidacion/src/app/app.module.ts
+++ Angular/Clase07/appFormularioValidacion/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [FormularioComponent]
 })
 export class AppModule { }
