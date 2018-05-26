19ddbc4589fce17ff47850381744236ad86eeb4c
Admin <admin@A51-04.local>
2018-05-26 13:47:14 -0500

    Clase16

---
+++ Node/Clase16/front/src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEventType } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+interface IRegistro {
+	titulo: string
+	descripcion?: string
+	foto?: any
+}
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+	registro: IRegistro
+	avance: number = 0
+
+	constructor(private http: HttpClient) { }
+
+	ngOnInit() {
+		this.registro = { titulo: "", descripcion: "", foto: "" }
+	}
+
+	grabar(titulo: string, descripcion: string) {
+		this.registro.titulo = titulo
+		this.registro.descripcion = descripcion
+
+		const fd: FormData = new FormData()
+		fd.append("titulo", this.registro.titulo)
+		fd.append("descripcion", this.registro.descripcion)
+		if (this.registro.foto) fd.append("foto", this.registro.foto)
+
+		const req = new HttpRequest("post", environment.apiRest, fd, {
+			reportProgress: true
+		})
+
+		this.http.request(req)
+			.subscribe(
+			evento => {
+				if (evento.type === HttpEventType.UploadProgress) {
+					this.avance = Math.floor(evento.loaded / evento.total * 100)
+					console.log(`Avance ${this.avance}`)
+				} else if (evento.type === HttpEventType.Response) {
+					console.log("Archivo subido")
+				} else {
+					console.log(evento.type)
+				}
+			},
+			error => console.log(error)
+			)
+
+
+		/*this.http.post(environment.apiRest, fd, {
+			observe: "body",
+			responseType: "json"
+		})
+			.subscribe(
+			data => console.log(data),
+			error => console.log(error)
+			)*/
+	}
+
+	archivoAgregado(data: any) {
+		const archivos: any = data.srcElement.files
+
+		this.registro.foto = archivos[0]
+	}
+}
---
+++ Node/Clase16/front/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiRest: "http://localhost:4000"
+};
